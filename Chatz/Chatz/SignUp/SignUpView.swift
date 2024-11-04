ecec762b3aa7611b6da161ef4fc0348979dc1e94
utkarshraj <utkarshraj@urbancompany.com>
2024-11-04 16:14:44 +0530

    Add firebase to project

---
+++ Chatz/Chatz/SignUp/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Chatz
+//
+//  Created by Utkarsh Raj on 04/11/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}

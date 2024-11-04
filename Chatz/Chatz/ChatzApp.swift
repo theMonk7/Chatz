94d499f30a56acc3b37334b57afe01b4675f7246
utkarshraj <utkarshraj@urbancompany.com>
2024-11-04 15:49:03 +0530

    Initial commit

---
+++ Chatz/Chatz/ChatzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatzApp.swift
+//  Chatz
+//
+//  Created by Utkarsh Raj on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
